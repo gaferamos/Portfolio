3e3afc31350d4bda0298536ea366a72a70ffc6d9
gaferamos <gabrielfelipe0252@gmail.com>
2025-09-11 15:09:53 -0300

    add portifolio files

---
+++ CardInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace gafe_portifolio
+{
+    public class CardInfoModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string CardResume { get; set; } = string.Empty;
+        public string CardFullInfo { get; set; } = string.Empty;
+        public string MinInfo { get; set; } = string.Empty;
+        public string RepoLink { get; set; } = string.Empty;
+        public string ImgSrc { get; set; } = string.Empty;
+    }
+}
