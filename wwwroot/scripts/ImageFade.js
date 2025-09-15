4ec90f3892f0f9b07ab087a6daaa61d8468751f1
gaferamos <gabrielfelipe0252@gmail.com>
2025-09-15 18:26:53 -0300

    new menu save navegatio position

--- wwwroot/scripts/ImageFade.js
+++ wwwroot/scripts/ImageFade.js
@@ -1,17 +1,17 @@
 ﻿window.imageFadeInit = () => {
-    const targets = document.querySelectorAll(".imageofme");
+    const targets = document.querySelectorAll(".card");
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("visible");
-                entry.target.classList.remove("hidden");
+                
             } else {
                 entry.target.classList.remove("visible");
-                entry.target.classList.add("hidden");
+                
             }
         });
-    }, { threshold: 0.6 });
+    }, { threshold: 0.2 });
 
     targets.forEach(el => observer.observe(el));
 };
---
+++ wwwroot/scripts/Toglescroll.js
@@ -0,0 +1,16 @@
+﻿window.toggleBodyScroll = {
+    add: () => {
+
+        // Salva a posição atual do scroll
+        window.scrollPosition = window.scrollY;
+        document.body.classList.add("noscroll");
+        document.body.style.top = `-${window.scrollPosition}px`;
+    },
+    remove: () => {
+        document.body.classList.remove("noscroll");
+        // Restaura a posição do scroll
+        window.scrollTo(0, window.scrollPosition);
+        document.body.style.top = '';
+    }
+};
+
